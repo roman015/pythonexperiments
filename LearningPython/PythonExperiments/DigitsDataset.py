e12635c58d4af5360a3ed9a3ca53dc4547c900e7
E0224940 <e0224940@u.nus.edu>
2017-08-19 18:39:42 +0800

    Cleaned up existing scripts a bit

--- LearningPython/PythonExperiments/DigitsDataset.py
+++ LearningPython/PythonExperiments/DigitsDataset.py
@@ -1,7 +1,10 @@
-import numpy
 import time
-from sklearn import svm
+import numpy
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn import datasets
+
+# Adjust these values to adjust the size of training and test sets
+numTest = -1500 # The last ones
 
 # Note the Start time
 start_time = time.clock()
@@ -15,14 +18,25 @@ print("\nData : \n" + str(digits["data"]) + "\nTarget : \n" + str(digits["target
 print("\nTarget Names : \n" + str(digits["target_names"]) + "\nimages : \n" + str(digits["images"]));
 print("\nDesc : \n" + str(digits["DESCR"]));
 
-# Create an svc and train it
-from sklearn.neighbors import KNeighborsClassifier
-clf = KNeighborsClassifier() #svm.SVC(gamma=0.001, C=100.)
-clf.fit(digits.data[:-1], digits.target[:-1])  
+# Create an clasifier and train it
+clf = KNeighborsClassifier()
+clf.fit(digits.data[:numTest], digits.target[:numTest])  
 
-# Try the svc on the last one
-print(str(clf.predict(digits.data[-1:])) + " | " + str(digits.target[-1:]));
+# Try the classifier on the last one in the dataset
+predicted = clf.predict(digits.data[numTest:])
+target = digits.target[numTest:]
 
+# Display the results
+print("Predicted : " + str(predicted) + "\nActual : " + str(digits.target[numTest:]));
+
+# Calculate the number of digits predicted correctly
+numSuccess = 0
+for pred,trgt in zip(predicted,target) :
+    if(pred == trgt) : 
+        numSuccess+=1
+
+print("\nSizeof Test Set : " + str(-1*numTest) + "\nSizeof Training Set : " + str(len(digits.data)+numTest) + "\n" )
+print("Success rate : " + str(numSuccess/len(target) * 100))
 # Note the End time and display the result
 end_time = time.clock()
 print("\nProgram took " + str(end_time-start_time) + " seconds");
--- LearningPython/PythonExperiments/Timing.py
+++ LearningPython/PythonExperiments/Timing.py
@@ -1,9 +1,19 @@
-import numpy
+"""
+Simple Program that uses time module to determine the runtime. 
+Doesn't do anything interesting, just code for reference.
+"""
 import time
+import numpy
 from sklearn import datasets
 
+# Start the clock
 start_time = time.clock()
+
+# Do something
 digits = datasets.load_digits()
+
+#End the Clock
 end_time = time.clock()
 
-print("DIGITS : \n" + str(digits.keys()) + "\nTime : " + str(end_time - start_time));
+#Calculate the time in seconds
+print("\nTime : " + str(end_time - start_time));
