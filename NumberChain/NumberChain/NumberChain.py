11fe011bac85ab55bc25b5d23d602f826bad60e1
Xavier Varricatt <xaviervarricatt@live.in>
2017-08-20 23:05:42 +0800

    Attempting with brute force method. Some lines have been commented, but it'll still mention the size of the solution found and the number of "excluded primes" in the set.

--- NumberChain/NumberChain/NumberChain.py
+++ NumberChain/NumberChain/NumberChain.py
@@ -1,12 +1,20 @@
 import numpy
 
+# Function to determine if given number is a prime
+def IsPrime(x) :
+    for i in range(2, x) :
+        if ( x % i == 0 ) :
+            return False
+    return True
+
+# Function to determine if two numbers can be linked
+def IsLinkable(a, b) :
+    return (a % b == 0) or (b % a == 0)
+
 # Function to Initialize the graph
 def CreateGraph(numSet, visited, neighbours) :
     # Sort numSet
     numSet = sorted(numSet);
-
-    # Initialize the visited graph (0 - Not visited, Non zero - Visited)
-    visited = numpy.zeros(len(numSet))
 
     # Setup Neighbours List
     for i in range(len(numSet)) :
@@ -14,21 +22,119 @@ def CreateGraph(numSet, visited, neighbours) :
         for j in range(len(numSet)) :
             if (i==j) : continue;
             elif ((numSet[i] % numSet[j] == 0) or (numSet[j] % numSet[i] == 0)) :
-                neighbours[i].append(numSet[j]);
+                neighbours[i].append(j);
 
-def BruteForce(numSet, visited, neighbours, curr) :
+# Brute Force Method
+def BruteForce(numSet, visited, neighbours, currIdx) :
+    result = visited[:]
+
+    # Make a backup
     currvisited = visited[:]
-    for i in range(curr, len(numSet)) :
-        #TODO : Complete this
-        
+
+    # Check all potential neighbours
+    for i in range(len(neighbours[currIdx])) :
+        # Get Index and value of potential neighbour
+        neighbourIdx = neighbours[currIdx][i]
+        neighbour = numSet[neighbourIdx]
+
+        # Perform Recursion
+        if (neighbour not in visited) and (IsLinkable(numSet[currIdx], neighbour)) :
+            # Mark number as visited
+            currvisited = visited[:]
+            currvisited.append(neighbour)
+
+            # Perform recursion
+            BruteForce(numSet, currvisited, neighbours, neighbourIdx)
+
+            # Update Result
+            if len(result) < len(currvisited) :
+                result = currvisited[:]
+
+    visited.clear()
+    for currVal in result :
+        visited.append(currVal)
+            
 
 # SCRIPT STARTS HERE
-numSet = range(1,11)
-visited = numpy.zeros(len(numSet))
-neighbours = [];
+problemSize = []
+solutionSize = []
+excludedPrimeSize = []
 
-CreateGraph(numSet, visited, neighbours)
+result = []
+numSet = range(1,1)
+visited = []
+neighbours = []
+prime = []
+excludedPrimes = []
 
-print("Numbers: " + str(numSet))
-print("Visited: " + str(visited))
-print("Neighbours : \n" + str(neighbours))
+# Trying out with multiple sets
+for setSize in range(1,23) :
+    # Reset values
+    result.clear()
+    numSet = range(1,setSize+1)
+    visited.clear()
+    neighbours.clear()
+    prime.clear()
+    excludedPrimes.clear()
+
+    # Fill the required data structures
+    CreateGraph(numSet, visited, neighbours)
+
+    # Find all the primes in the list
+    for currNum in numSet :
+        if IsPrime(currNum) :
+            prime.append(currNum)
+            if (currNum * 2 > setSize) :
+                excludedPrimes.append(currNum)
+
+    # Display input
+    print("Testing with %s numbers" % len(numSet))
+    #print("Numbers: " + str(numSet))
+    #print("Visited: " + str(visited))
+    #print("Neighbours : \n" + str(neighbours))
+    #print()
+    #print()
+    #print("------------------");
+
+    # Start the recursion
+    for currStart in range(len(numSet)) :
+        visited.clear()
+        visited.append(numSet[currStart])
+        BruteForce(numSet, visited, neighbours, currStart)
+        # Update Result
+        #if  len(visited) >= (len(numSet) - len(excludedPrimes) + 1) :
+        #    print("Potential Chain: " + str(visited))
+        #    print("Potential Len: " + str(len(visited)))
+        if len(visited) > len(result) :
+            result.clear()
+            for currVal in visited :
+                result.append(currVal)        
+
+    # Display the result
+    #print("------------------");
+    #print()
+    #print()
+    #print("Biggest Chain: " + str(result))
+    print("Length of Chain: " + str(len(result)))
+    #print()
+    #print("Sorted: " + str(sorted(result)))
+    #print()
+
+    # Display the primes and excluded primes
+    #print("Primes: " + str(prime))
+    #print("Excluded Primes: " + str(excludedPrimes))
+    #print()
+    print("There are %s Excluded Primes"%len(excludedPrimes))
+    #print()
+    print("------------------");
+
+    problemSize.append(setSize)
+    solutionSize.append(len(result))
+    excludedPrimeSize.append(len(excludedPrimes))
+
+# End of loop
+print(str(problemSize))
+print()
+print(str(solutionSize))
+print()
+print(str(excludedPrimeSize))
--- NumberChain/Readme.md
+++ NumberChain/Readme.md
@@ -8,4 +8,11 @@ The idea is to form the biggest chain using the numbers from 1 - 100 such that f
 
 Problem first encountered here : https://fivethirtyeight.com/features/pick-a-number-any-number/
 
-I'll need to brush up on my TSP (https://en.wikipedia.org/wiki/Travelling_salesman_problem), among other things for this...
+***Observations***
+
+These are unconfirmed observations based on smaller sets, further research required:
+
+1. Prime numbers whose first multiple is outside the set is always excluded (except for one of them, which is always paired with 1)
+2. Solution usually has a 1 paired with a prime number at one end of the path
+3. It's possible to speculate then that it's impossible to form a cycle with such a chain of numbers
+4. Prime numbers can only connect to their multiples (This might seem obvious, but this fact should be helpful when it comes to generating solutions in a cheesy manner)
