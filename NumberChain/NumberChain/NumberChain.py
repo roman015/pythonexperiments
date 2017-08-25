64db4313c328dfde1fc004cf707b9ce50b2aa074
E0224940 <e0224940@u.nus.edu>
2017-08-25 14:35:50 +0800

    Code now displays all generated solutions properly

--- NumberChain/NumberChain/NumberChain.py
+++ NumberChain/NumberChain/NumberChain.py
@@ -27,10 +27,12 @@ def CreateGraph(numSet, visited, neighbours) :
 
 # Global variable to count number of solutions found
 NumberOfSolutions = 0
+Solutions = []
 
 # Brute Force Method
 def BruteForce(numSet, visited, neighbours, currIdx, filterOutputSize) :
     global NumberOfSolutions
+    global Solutions
     result = visited[:]
 
     # Make a backup
@@ -56,11 +58,11 @@ def BruteForce(numSet, visited, neighbours, currIdx, filterOutputSize) :
                 result = currvisited[:]
 
             # Print if the solution is large enough
-            if len(result) >= filterOutputSize :
+            if len(result) >= filterOutputSize and result not in Solutions:
                 NumberOfSolutions = NumberOfSolutions + 1
                 filterOutputSize = len(result)
-                print("Potential Chain: " + str(result))
-                print("Potential Length:" + str(len(result)))
+                Solutions.append(result)
+                print(str(result))
 
     visited.clear()
     for currVal in result :
@@ -69,35 +71,32 @@ def BruteForce(numSet, visited, neighbours, currIdx, filterOutputSize) :
 # Solve the problem for a specific size
 def Solve(setSize, outputSize) :
     global NumberOfSolutions
+    global Solutions
     problemSize = []
     solutionSize = []
-    excludedPrimeSize = []
 
     result = []
     numSet = range(1,setSize + 1)
     visited = []
     neighbours = []
-    prime = []
-    excludedPrimes = []
 
-    # Reset the Global Counter
+    # Reset the Global Counter and Solutions list
     NumberOfSolutions = 0
+    Solutions.clear();
 
     # Fill the required data structures
     CreateGraph(numSet, visited, neighbours)
                 
-    # Find all the primes in the list
-    for currNum in numSet :
-        if IsPrime(currNum) :
-            prime.append(currNum)
-            if (currNum * 2 > setSize) :
-                excludedPrimes.append(currNum)
-
-    # Start the recursion
-    for currStart in range(len(numSet)) :
+    # Start the recursion with each element as the starting number
+    for currStart in range(len(numSet)) :    
+        #Place the starting number
         visited.clear()
         visited.append(numSet[currStart])
+        
+        # Call Recursively
         BruteForce(numSet, visited, neighbours, currStart, outputSize)
+        
+        # Replace the solution and solution size
         if outputSize < len(visited) : 
             outputSize = len(visited)
         if len(visited) > len(result) :
@@ -109,32 +108,13 @@ def Solve(setSize, outputSize) :
     return  result
 
 # SCRIPT STARTS HERE
-ExpectedSizes = [1, 2, 3, 4, 4, 6, 6, 7, 8, 9, 9, 11, 11, 12, 13, 14, 14, 16, 16, 17, 18, 19, 19, 21, 21, 22, 23, 24, 24]
-
-#inputSize  = 2
-#outputSize = 2
-#while inputSize < 101 :
-#   inputSize = inputSize + 1
-
-#   print("Output : \t" + str(outputSize) + "\tInput : \t" + str(inputSize))
-#   outputSize = outputSize + 1
-
-#   if ((outputSize) % 5 == 0) :
-#       print("Output : \t" + str(outputSize-1) + "\tInput : \t" + str(inputSize+1))
-#       outputSize = outputSize  + 1
-#       print("Output : \t" + str(outputSize) + "\tInput : \t" + str(inputSize+2))
-#       inputSize = inputSize + 2
-
-    
+ExpectedSizes = [1, 2, 3, 4, 4, 6, 6, 7, 8, 9, 9, 11, 11, 12, 13, 14, 14, 16, 16, 17, 18, 19, 19, 21, 21, 22, 23, 24, 24, 26, 26, 27, 28, 29, 29, 31, 31]
 
 total_start_time = time.time()
-for problemSize in range (1, 10) :
-    #maxSolution = 1
+for problemSize in range (1, 31) :
     solution = []
     start_time = time.time()
     solution = Solve(problemSize, ExpectedSizes[problemSize-1])
-    #if len(solution) > maxSolution : 
-    #    maxSolution = len(solution)
     end_time = time.time()
     print()
     print("Problem Size : " + str(problemSize))
